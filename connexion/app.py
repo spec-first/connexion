1e40fb8ea628c41208afc63b8b50b9b753c880da
João Santos <joao.santos@zalando.de>
2015-06-08 09:20:36 +0200

    https://github.com/zalando/connexion/issues/1 Don't crash if arguments are not provided on the api

--- connexion/app.py
+++ connexion/app.py
@@ -68,6 +68,7 @@ class App:
         """
         logger.debug('Adding API: %s', swagger_file)
         # TODO test if base_url starts with an / (if not none)
+        arguments = arguments or dict()
         arguments = dict(self.arguments, **arguments)  # copy global arguments and update with api specfic
         yaml_path = self.specification_dir / swagger_file
         api = connexion.api.Api(yaml_path, base_path, arguments)
