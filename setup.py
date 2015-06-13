b40295aa48a3e9d2134dadf622d2b4e904491eb2
João Santos <jmcs@jsantos.eu>
2015-06-13 11:27:44 +0200

    https://github.com/zalando/connexion/issues/6 Test utils

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 from connexion.version import version
+
+
+class PyTest(TestCommand):
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.cov = None
+        self.pytest_args = ['--cov', 'connexion', '--cov-report', 'term-missing']
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -16,6 +37,8 @@ setup(
     url='https://github.com/zalando/connexion',
     license='Apache License Version 2.0',
     install_requires=['flask', 'PyYAML', 'tornado', 'requests'],
+    tests_require=['pytest-cov', 'pytest'],
+    cmdclass={'test': PyTest},
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
