192739ffcc8dc5cc1abbd090160209a400d46c8d
Henning Jacobs <henning@jacobs1.de>
2015-07-17 09:36:04 +0200

    support paths with hyphen

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,8 @@ import connexion.utils as utils
 def test_flaskify_path():
     assert utils.flaskify_path("{test-path}") == "<test_path>"
     assert utils.flaskify_path("api/{test-path}") == "api/<test_path>"
+    assert utils.flaskify_path("my-api/{test-path}") == "my-api/<test_path>"
+    assert utils.flaskify_path("foo_bar/{a-b}/{c_d}") == "foo_bar/<a_b>/<c_d>"
 
 
 def test_flaskify_endpoint():
