52171ce21546cf9d65eb9d40907ebe1a5ba1fecf
Henning Jacobs <henning@jacobs1.de>
2015-10-29 13:54:40 +0100

    Bumped version to 0.12.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '0.12.0'
+version = '0.12.1'
 py_major_version, py_minor_version, _ = (int(v.rstrip('+')) for v in platform.python_version_tuple())
 
 requires = ['flask', 'PyYAML', 'requests', 'six', 'strict-rfc3339']
