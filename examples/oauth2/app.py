fcb20c7e056e5a257015fac4ceeeed515abb9d42
hjacobs <henning@jacobs1.de>
2016-01-22 19:36:50 +0100

    #123 add OAuth2 example with mock tokeninfo endpoint

---
+++ examples/oauth2/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Basic example of a resource server
+'''
+
+import connexion
+import flask
+
+
+def get_secret() -> str:
+    # the token's uid will be set in request.user
+    return 'You are: {uid}'.format(uid=flask.request.user)
+
+if __name__ == '__main__':
+    app = connexion.App(__name__)
+    app.add_api('app.yaml')
+    app.run(port=8080)
