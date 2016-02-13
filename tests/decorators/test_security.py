b2caf7e9fc45daf6b86b91df21b4c978568315ed
hjacobs <henning@jacobs1.de>
2016-02-13 13:06:50 +0100

    #146 add test for invalid Authorization header

--- tests/decorators/test_security.py
+++ tests/decorators/test_security.py
@@ -1,5 +1,6 @@
 from mock import MagicMock
-from connexion.decorators.security import get_tokeninfo_url
+from connexion.decorators.security import get_tokeninfo_url, verify_oauth
+from connexion.problem import problem
 
 def test_get_tokeninfo_url(monkeypatch):
     env = {}
@@ -12,3 +13,17 @@ def test_get_tokeninfo_url(monkeypatch):
     assert get_tokeninfo_url(security_def) == 'foo'
     security_def = {'x-tokenInfoUrl': 'bar'}
     assert get_tokeninfo_url(security_def) == 'bar'
+
+
+def test_verify_oauth_invalid_auth_header(monkeypatch):
+    def func():
+        pass
+
+    wrapped_func = verify_oauth('https://example.org/tokeninfo', set(['admin']), func)
+
+    request = MagicMock()
+    app = MagicMock()
+    monkeypatch.setattr('connexion.decorators.security.request', request)
+    monkeypatch.setattr('flask.current_app', app)
+    resp = wrapped_func()
+    assert resp == problem(401, 'Unauthorized', 'Invalid authorization header')
