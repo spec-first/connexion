6fe5cdc0ad285a989fd6943ca8e53366ad343920
David Hotham <david.hotham@metaswitch.com>
2016-03-29 12:31:07 +0100

    Python3 compatibility

--- tests/fakeapi/hello.py
+++ tests/fakeapi/hello.py
@@ -244,7 +244,7 @@ def test_formdata_missing_param():
 
 def test_formdata_file_upload(formData):
     filename = formData.filename
-    contents = formData.read()
+    contents = formData.read().decode('utf-8')
     return {filename: contents}
 
 
