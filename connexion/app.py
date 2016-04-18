b246331c84d905da358e4f0832be9cb7e1ff1d85
Timo Puschkasch <timo@puschkasch.com>
2016-04-18 13:32:59 +0200

    Removed whitespace from blank lines due to TravisCI failure

--- connexion/app.py
+++ connexion/app.py
@@ -271,12 +271,11 @@ class App(object):
             http_server.serve_forever()
         else:
             raise Exception('Server %s not recognized', self.server)
-            
+ 
     def __call__(self, environ, start_response):
         """
         Makes the class callable to be WSGI-compliant. As Flask is used to handle requests,
         this is a passthrough-call to the Flask callable class.
-        
         This is an abstraction to avoid directly referencing the app attribute from outside the
         class and protect it from unwanted modification.
         """
