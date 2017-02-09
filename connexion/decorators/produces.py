c6b015291273322efd1a04d362e6f7b1ed89e288
Leena Bhegade <bhegde.leena@gmail.com>
2017-02-09 18:26:46 +0000

    Use six package for conditional checking

--- connexion/decorators/produces.py
+++ connexion/decorators/produces.py
@@ -2,6 +2,7 @@
 import datetime
 import functools
 import logging
+import six
 
 import flask
 from flask import json
@@ -76,7 +77,12 @@ class Jsonifier(BaseSerializer):
         """ Central point where JSON serialization happens inside
         Connexion.
         """
-        return "{}\n".format(json.dumps(data, indent=2, encoding="utf-8"))
+        if six.PY2 == True:
+            encoding = "utf-8"
+        else:
+            encoding = None
+
+        return "{}\n".format(json.dumps(data, indent=2, encoding=encoding))
 
     def __call__(self, function):
         """
