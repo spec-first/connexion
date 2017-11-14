4b00ede7678979c2bfb50cba4650302d05c25fe8
Mikko Valkonen <msvalkon@gmail.com>
2017-11-14 10:43:12 +0200

    add a test to validate form data params

--- tests/decorators/test_parameter.py
+++ tests/decorators/test_parameter.py
@@ -1,6 +1,8 @@
+
 from connexion.decorators.parameter import parameter_to_arg
 # we are using "mock" module here for Py 2.7 support
 from mock import MagicMock
+from testfixtures import LogCapture
 
 
 def test_injection(monkeypatch):
@@ -17,3 +19,22 @@ def test_injection(monkeypatch):
     parameter_to_arg({}, [], handler)(request)
 
     func.assert_called_with(p1='123')
+
+
+def test_query_sanitazion(query_sanitazion):
+    app_client = query_sanitazion.app.test_client()
+    l = LogCapture()
+
+    url = '/v1.0/greeting'
+    response = app_client.post(url, data={'name': 'Jane Doe'})
+    # This is ugly. The reason for asserting the logging in this way
+    # is that in order to use LogCapture().check, we'd have to assert that
+    # a specific sequence of logging has occurred. This is too restricting
+    # for future development, and we are really only interested in the fact
+    # a single message is logged.
+    messages = [x.strip() for x in str(l).split("\n")]
+    assert "FormData parameter 'name' in function arguments" in messages
+    assert "Query Parameter 'name' in function arguments" not in messages
+    assert "Function argument 'name' not defined in specification" not in messages
+    assert response.status_code == 200
+    l.uninstall()
