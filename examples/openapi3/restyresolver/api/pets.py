116e15cf5a0e74193c1a477918b25be055a17fad
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-04-26 00:36:23 -0700

    add oas3 restyresolver example

---
+++ examples/openapi3/restyresolver/api/pets.py
@@ -0,0 +1,46 @@
+import datetime
+
+from connexion import NoContent
+
+pets = {}
+
+
+def post(name, tag=None):
+    count = len(pets)
+    pet = {}
+    pet['id'] = count + 1
+    pet["tag"] = tag
+    pet["name"] = name
+    pet['last_updated'] = datetime.datetime.now()
+    pets[pet['id']] = pet
+    return pet, 201
+
+
+def put(id, name, tag=None):
+    id = int(id)
+    pet = pets.get(id, {"id": id})
+    pet["name"] = name
+    pet["tag"] = tag
+    pet['last_updated'] = datetime.datetime.now()
+    pets[id] = pet
+    return pets[id]
+
+
+def delete(id):
+    id = int(id)
+    if pets.get(id) is None:
+        return NoContent, 404
+    del pets[id]
+    return NoContent, 204
+
+
+def get(petId):
+    id = int(petId)
+    if pets.get(id) is None:
+        return NoContent, 404
+    return pets[id]
+
+
+def search(limit=100):
+    # NOTE: we need to wrap it with list for Python 3 as dict_values is not JSON serializable
+    return list(pets.values())[0:limit]
