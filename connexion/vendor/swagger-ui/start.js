3b3cdc8c04ef5cd3e7403da441c842c9ddef9296
David <dscharf@gmx.net>
2018-06-07 13:41:01 +0200

    change loading of base swagger file

---
+++ connexion/vendor/swagger-ui/start.js
@@ -0,0 +1,56 @@
+$(function () {
+
+  var url = window.location.search.match(/url=([^&]+)/);
+  if (url && url.length > 1) {
+    url = decodeURIComponent(url[1]);
+  } else {
+    url = window.location.pathname + "../swagger.json";
+  }
+
+  hljs.configure({
+    highlightSizeThreshold: 5000
+  });
+
+  // Pre load translate...
+  if(window.SwaggerTranslator) {
+    window.SwaggerTranslator.translate();
+  }
+  window.swaggerUi = new SwaggerUi({
+    url: url,
+    // https://github.com/zalando/connexion/issues/110
+    validatorUrl: null,
+    dom_id: "swagger-ui-container",
+    supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+    onComplete: function(swaggerApi, swaggerUi){
+      if(typeof initOAuth == "function") {
+        initOAuth({
+          clientId: "your-client-id",
+          clientSecret: "your-client-secret-if-required",
+          realm: "your-realms",
+          appName: "your-app-name",
+          scopeSeparator: " ",
+          additionalQueryStringParams: {}
+        });
+      }
+
+      if(window.SwaggerTranslator) {
+        window.SwaggerTranslator.translate();
+      }
+    },
+    onFailure: function(data) {
+      log("Unable to Load SwaggerUI");
+    },
+    docExpansion: "none",
+    jsonEditor: false,
+    defaultModelRendering: 'schema',
+    showRequestHeaders: false
+  });
+
+  window.swaggerUi.load();
+
+  function log() {
+    if ('console' in window) {
+      console.log.apply(console, arguments);
+    }
+  }
+});
