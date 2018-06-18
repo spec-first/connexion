144629095d9d23be3dec8d75a97c5b3b9a1eaa2d
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-06-17 22:41:19 -0700

    flake

--- connexion/query_parsing.py
+++ connexion/query_parsing.py
@@ -5,13 +5,14 @@ QUERY_STRING_DELIMITERS = {
     'form': ','
 }
 
+
 def resolve_query_duplicates(values, param_defn):
     """ Resolve cases where query parameters are provided multiple times.
         The default behavior is to use the last-defined value.
-        For example, if the query string is '?a=1,2,3&a=4,5,6' the value of 
+        For example, if the query string is '?a=1,2,3&a=4,5,6' the value of
         `a` would be "4,5,6".
         However, if 'explode' is true, or the 'collectionFormat' is 'multi'
-        (swagger2) then the duplicate values are concatenated together and 
+        (swagger2) then the duplicate values are concatenated together and
         `a` would be "1,2,3,4,5,6".
     """
     try:
@@ -23,11 +24,11 @@ def resolve_query_duplicates(values, param_defn):
         if explode:
             return delimiter.join(values)
     except KeyError:
-        # swagger2, or undefined
+        # swagger2
         if param_defn.get('collectionFormat') == 'multi':
             return ','.join(values)
+    # default to last defined value
     return values[-1]
-
 
 
 def query_split(value, param_defn):
@@ -37,12 +38,10 @@ def query_split(value, param_defn):
         # oas3
         style = param_defn['style']
         delimiter = QUERY_STRING_DELIMITERS.get(style, ',')
-        is_form = (style == 'form')
-        explode = param_defn.get('explode', is_form)
         return value.split(delimiter)
     except KeyError:
-        # swagger2, or undefined
-        if (param_defn.get('collectionFormat') == 'pipes'):
+        # swagger2
+        if param_defn.get('collectionFormat') == 'pipes':
             return value.split('|')
-        else:  # default: csv
-            return value.split(',')
+    # default: csv
+    return value.split(',')
