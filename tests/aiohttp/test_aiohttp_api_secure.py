7d93284507492d5dacff899f92e182f1a153c380
João Santos <joao@zalando.de>
2018-09-27 13:22:59 +0200

    Revert "Use aiohttp_client for aiohttp tests"
    This reverts commit 9d69cde222ae60c4509451a5aa2a9cf94b374500.

--- tests/aiohttp/test_aiohttp_api_secure.py
+++ tests/aiohttp/test_aiohttp_api_secure.py
@@ -1,19 +1,18 @@
 import asyncio
 
 import ujson
-from aiohttp.test_utils import aiohttp_client
 from conftest import TEST_FOLDER
 from connexion import AioHttpApp
 
 
 @asyncio.coroutine
-def test_auth_all_paths(oauth_requests, aiohttp_api_spec_dir):
+def test_auth_all_paths(oauth_requests, aiohttp_api_spec_dir, test_client):
     app = AioHttpApp(__name__, port=5001,
                      specification_dir=aiohttp_api_spec_dir,
                      debug=True, auth_all_paths=True)
     app.add_api('swagger_secure.yaml')
 
-    app_client = yield from aiohttp_client(app.app)
+    app_client = yield from test_client(app.app)
 
     headers = {'Authorization': 'Bearer 100'}
     get_inexistent_endpoint = yield from app_client.get(
@@ -31,13 +30,13 @@ def test_auth_all_paths(oauth_requests, aiohttp_api_spec_dir):
 
 
 @asyncio.coroutine
-def test_secure_app(oauth_requests, aiohttp_api_spec_dir):
+def test_secure_app(oauth_requests, aiohttp_api_spec_dir, test_client):
     # Create the app and run the test_app testcase below.
     app = AioHttpApp(__name__, port=5001,
                      specification_dir=aiohttp_api_spec_dir,
                      debug=True)
     app.add_api('swagger_secure.yaml')
-    app_client = yield from aiohttp_client(app.app)
+    app_client = yield from test_client(app.app)
 
     post_hello = yield from app_client.post('/v1.0/greeting/jsantos')
     assert post_hello.status == 401
