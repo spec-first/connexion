93bb88df1210b9e68c96e0e9488e1a24aab76c43
Daniel Grossmann-Kavanagh <me@danielgk.com>
2019-01-08 16:44:32 -0800

    use asyncio.coroutine instead of async def

--- connexion/apis/aiohttp_api.py
+++ connexion/apis/aiohttp_api.py
@@ -128,7 +128,8 @@ class AioHttpApi(AbstractAPI):
                 self._get_swagger_ui_home
             )
 
-        async def redirect(request):
+        @asyncio.coroutine
+        def redirect(request):
             raise web.HTTPMovedPermanently(location=self.base_path + console_ui_path + '/')
         self.subapp.router.add_route(
             'GET',
