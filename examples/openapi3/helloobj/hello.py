700ab526ed3f6b77dc82df740d4ab3ff44e1843a
Prabhu Subramanian <prabhu.subramanian@bp.com>
2019-03-04 21:16:21 +0000

    Added object post example to demo the error

---
+++ examples/openapi3/helloobj/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import connexion
+
+
+def post_greeting(body: dict) -> dict:
+    return {"name": body["name"]}
+
+if __name__ == '__main__':
+    app = connexion.FlaskApp(__name__, port=9090, specification_dir='openapi/')
+    app.add_api('helloworld-api.yaml', arguments={'title': 'Hello World Example'})
+    app.run()
