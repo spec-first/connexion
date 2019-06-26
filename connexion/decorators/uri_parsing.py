0797f3631910ae31bae9461b7df7bf0c8638d139
Davy Durham <ddurham@users.sourceforge.net>
2019-06-26 01:06:04 -0500

    [#935] adding support to automatically decode json when a multipart/form-data contains a field with an application/json content-type

--- connexion/decorators/uri_parsing.py
+++ connexion/decorators/uri_parsing.py
@@ -2,6 +2,7 @@
 import abc
 import functools
 import logging
+import json
 
 import six
 
@@ -174,6 +175,8 @@ class OpenAPIURIParser(AbstractURIParser):
                 self._resolve_param_duplicates(form_data[k], encoding, 'form')
             if defn and defn["type"] == "array":
                 form_data[k] = self._split(form_data[k], encoding, 'form')
+            elif encoding.get('contentType', None) == 'application/json':
+                form_data[k] = json.loads(form_data[k])
         return form_data
 
     def resolve_query(self, query_data):
