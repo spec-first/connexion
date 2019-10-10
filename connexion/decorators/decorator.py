593152fa766b1cb1c3aac5be93a575ff175bf9c1
Heiss <52699794+Heiss@users.noreply.github.com>
2019-10-10 17:40:49 +0200

    implements jaeger tracing

--- connexion/decorators/decorator.py
+++ connexion/decorators/decorator.py
@@ -1,7 +1,7 @@
 import functools
 import logging
 
-from ..utils import has_coroutine
+from ..utils import has_coroutine, jaeger
 
 logger = logging.getLogger('connexion.decorators.decorator')
 
@@ -37,6 +37,20 @@ class RequestResponseDecorator(BaseDecorator):
         :type function: types.FunctionType
         :rtype: types.FunctionType
         """
+        
+        # if jaeger is configured, then start a span now
+        if jaeger:
+            from opentracing.ext import tags
+            
+            # extract the context from request header to continue a session
+            # taken from https://github.com/yurishkuro/opentracing-tutorial/tree/master/python/lesson03#extract-the-span-context-from-the-incoming-request-using-tracerextract
+            request = self.api.get_request(*args, **kwargs)
+            span_ctx = tracer.extract(Format.HTTP_HEADERS, request.headers)
+            span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
+            
+            span = tracer.start_active_span('RequestResponseDecorator', child_of=span_ctx, tags=span_tags)
+            span.log_kv({"request": request}
+                                        
         if has_coroutine(function, self.api):  # pragma: 2.7 no cover
             from .coroutine_wrappers import get_request_life_cycle_wrapper
             wrapper = get_request_life_cycle_wrapper(function, self.api, self.mimetype)
@@ -48,4 +62,9 @@ class RequestResponseDecorator(BaseDecorator):
                 response = function(request)
                 return self.api.get_response(response, self.mimetype, request)
 
+        # if jaeger and a span are configured, finish it now.
+        if jaeger and span:
+            span.log_kv({"response": wrapper.response}
+            span.finish()
+            
         return wrapper
