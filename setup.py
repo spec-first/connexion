a9ad272882e1e87f7650069c26f99239771f0b5b
Henning Jacobs <henning@zalando.de>
2019-10-18 12:50:39 +0200

    drop Python 3.5, add 3.8

--- setup.py
+++ setup.py
@@ -118,9 +118,9 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
