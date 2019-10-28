6ad73bd75299f60e3d8a34cfca1e55af7a5b4daa
Peter Heiss <peter.heiss@exchange.wwu.de>
2019-10-28 10:16:08 +0100

    remove condition and add a comment

--- connexion/decorators/TracerDecorator.py
+++ connexion/decorators/TracerDecorator.py
@@ -6,34 +6,31 @@ logger = logging.getLogger('')
 
 def TracerDecorator(func):
     def wrapper(response, mimetype=None, request=None):
+        #don't need to check, because this Decorator will only be executed, if the tracer was configured at startup
         tracer = get_tracer()
 
-        # if tracer is configured, then start a span now
-        if tracer is not None:
-            from opentracing.ext import tags
-            from opentracing.propagation import Format
+        from opentracing.ext import tags
+        from opentracing.propagation import Format
 
-            # extract the context from request header to continue a session
-            # taken from https://github.com/yurishkuro/opentracing-tutorial/tree/master/python/lesson03#extract-the-span-context-from-the-incoming-request-using-tracerextract
-            if request is not None:
-                span_ctx = tracer.extract(Format.HTTP_HEADERS, request.headers)
-                span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
+        # extract the context from request header to continue a session
+        # taken from https://github.com/yurishkuro/opentracing-tutorial/tree/master/python/lesson03#extract-the-span-context-from-the-incoming-request-using-tracerextract
+        if request is not None:
+            span_ctx = tracer.extract(Format.HTTP_HEADERS, request.headers)
+            span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
 
-                # remove domain from url, so only the path is in the span
-                from urllib.parse import urlparse
-                path = urlparse(request.url).path
+            # remove domain from url, so only the path is in the span
+            from urllib.parse import urlparse
+            path = urlparse(request.url).path
 
-                scope = tracer.start_span(request.method + "_" + path, child_of=span_ctx, tags=span_tags)
-                scope.log_kv({"request": request})
-            else:
-                scope = tracer.start_span("NO_REQUEST_CONTEXT")
-
-            resp = func(response, mimetype, request)
-
-            scope.log_kv({"response": response})
-            scope.finish()
+            scope = tracer.start_span(request.method + "_" + path, child_of=span_ctx, tags=span_tags)
+            scope.log_kv({"request": request})
         else:
-            resp = func(response, mimetype, request)
+            scope = tracer.start_span("NO_REQUEST_CONTEXT")
+
+        resp = func(response, mimetype, request)
+
+        scope.log_kv({"response": response})
+        scope.finish()
 
         return resp
 
