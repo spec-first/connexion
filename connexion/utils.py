874d96abaf5f611be50c4d4fe5575c5007bdb1a0
Malcolm Smith <malcolm.smith@nccgroup.trust>
2020-04-01 12:27:33 +0100

    Refactor to avoid copyright infringement highlighted by a Black Duck scan.  See https://github.com/zalando/connexion/issues/1200

--- connexion/utils.py
+++ connexion/utils.py
@@ -61,10 +61,11 @@ def deep_merge(a, b):
 def deep_getattr(obj, attr):
     """
     Recurses through an attribute chain to get the ultimate value.
-
-    Stolen from http://pingfive.typepad.com/blog/2010/04/deep-getattr-python-function.html
     """
-    return functools.reduce(getattr, attr.split('.'), obj)
+
+    attrs = attr.split('.')
+
+    return functools.reduce(getattr, attrs, obj)
 
 
 def deep_get(obj, keys):
