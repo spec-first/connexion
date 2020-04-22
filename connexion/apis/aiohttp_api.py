2af3c4825e9b9ceb4dce452a7f62723ad5c0247d
Davy Durham <ddurham@users.sourceforge.net>
2020-04-21 23:37:30 -0500

    now splitting out multipart POSTs into files[] and form[], handling duplicate keys as the rest of connexion expects     - Based parly on existing PR: https://github.com/zalando/connexion/pull/987

--- connexion/apis/aiohttp_api.py
+++ connexion/apis/aiohttp_api.py
@@ -300,13 +300,50 @@ class AioHttpApi(AbstractAPI):
         :rtype: ConnexionRequest
         """
         url = str(req.url)
-        logger.debug('Getting data and status code',
-                     extra={'has_body': req.has_body, 'url': url})
+
+        logger.debug(
+            'Getting data and status code',
+            extra={
+                # has_body | can_read_body report if
+                # body has been read or not
+                # body_exists refers to underlying stream of data
+                'body_exists': req.body_exists,
+                'can_read_body': req.can_read_body,
+                'content_type': req.content_type,
+                'url': url,
+            },
+        )
 
         query = parse_qs(req.rel_url.query_string)
         headers = req.headers
         body = None
-        if req.body_exists:
+
+        # if request is not multipart, `data` will be empty dict
+        # and stream will not be consumed
+        post_data = yield from req.post()
+
+        # set those up beforehand, they are needed anyway
+        files = {}
+        form = {}
+
+        if post_data:
+            logger.debug('Reading multipart data from request')
+            for k, v in post_data.items():
+                if isinstance(v, web.FileField):
+                    if k in files:
+                        try:
+                            files[k].append(v)
+                        except AttributeError:
+                            files[k] = [files[k], v]
+                    else:
+                        files[k] = v
+                else:
+                    # put normal fields as an array, that's how werkzeug does that for Flask
+                    # and that's what Connexion expects in its processing functions
+                    form[k] = [v]
+            body = b''
+        else:
+            logger.debug('Reading data from request')
             body = yield from req.read()
 
         return ConnexionRequest(url=url,
@@ -316,7 +353,8 @@ class AioHttpApi(AbstractAPI):
                                 headers=headers,
                                 body=body,
                                 json_getter=lambda: cls.jsonifier.loads(body),
-                                files={},
+                                form=form,
+                                files=files,
                                 context=req)
 
     @classmethod
