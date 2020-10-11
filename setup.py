ba9a91ba6265866c110a6e098e954969b25eb381
Patrick Wang <patrick@covar.com>
2020-10-11 11:17:52 -0400

    Add explicit werkzeug dependency
    Fixes #1149.

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ install_requires = [
     'requests>=2.9.1',
     'inflection>=0.3.1',
     'openapi-spec-validator>=0.2.4',
+    'werkzeug>=1.0,<2.0',
 ]
 
 swagger_ui_require = 'swagger-ui-bundle>=0.0.2'
