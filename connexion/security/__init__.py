d8dfd6c5252e6b1eef47abe229d6f3e420d150d5
Ruwan Lambrichts <ruwan.lambrichts@ml6.eu>
2021-07-04 10:54:28 +0200

    Catch importerror in security init

--- connexion/security/__init__.py
+++ connexion/security/__init__.py
@@ -3,3 +3,24 @@
 # abstract
 from .async_security_handler_factory import AbstractAsyncSecurityHandlerFactory  # NOQA
 from .security_handler_factory import AbstractSecurityHandlerFactory  # NOQA
+
+
+def not_installed_error(exc):  # pragma: no cover
+    import functools
+
+    def _required_lib(exc, *args, **kwargs):
+        raise exc
+
+    return functools.partial(_required_lib, exc)
+
+
+# concrete
+try:
+    from .flask_security_handler_factory import FlaskSecurityHandlerFactory
+except ImportError as err:  # pragma: no cover
+    FlaskSecurityHandlerFactory = not_installed_error(err)
+
+try:
+    from .aiohttp_security_handler_factory import AioHttpSecurityHandlerFactory
+except ImportError as err:  # pragma: no cover
+    AioHttpSecurityHandlerFactory = not_installed_error(err)
