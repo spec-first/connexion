1a7d0896d6fa69c9bd39767c7bc3ae6c1b6edd89
Robbe Sneyders <robbe.sneyders@ml6.eu>
2021-07-06 09:43:04 +0200

    Remove deprecated static analysis (#1376)
    * Remove deprecated static analysis config
    
    * Remove Zappr PR rules

--- setup.py
+++ setup.py
@@ -57,8 +57,7 @@ class PyTest(TestCommand):
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.cov = None
-        self.pytest_args = ['--cov', 'connexion', '--cov-report', 'term-missing',
-                            '--cov-config=py3-coveragerc', '-v']
+        self.pytest_args = ['--cov', 'connexion', '--cov-report', 'term-missing', '-v']
         self.cov_html = False
 
     def finalize_options(self):
