e201991692528559fbec2b55f53cb2c98e0c29c9
Miguel G. Flores <miguel.gonzalez@feverup.com>
2021-07-07 18:26:18 +0200

    Use dict.get

--- connexion/decorators/response.py
+++ connexion/decorators/response.py
@@ -54,7 +54,7 @@ class ResponseValidator(BaseDecorator):
         if response_definition and response_definition.get("headers"):
             # converting to set is needed to support python 2.7
             required_header_keys = {k for (k, v) in response_definition.get("headers").items()
-                                    if 'required' not in v or v['required']}
+                                    if v.get('required', False)}
             header_keys = set(headers.keys())
             missing_keys = required_header_keys - header_keys
             if missing_keys:
