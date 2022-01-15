859078be3f5e8fe733ac0291c28c7ac9ed42f449
Robbe Sneyders <robbe.sneyders@ml6.eu>
2022-01-15 23:34:52 +0100

    Bump upper boundry for jsonschema version to 5.0.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ version = read_version('connexion')
 
 install_requires = [
     'clickclick>=1.2,<21',
-    'jsonschema>=2.5.1,<4',
+    'jsonschema>=2.5.1,<5',
     'PyYAML>=5.1,<6',
     'requests>=2.9.1,<3',
     'inflection>=0.3.1,<0.6',
