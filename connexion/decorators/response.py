d375ea8daca64de4b422452c1b68992e979f075a
Chris HORLER <cshorler@googlemail.com>
2022-02-19 16:18:45 +0000

    Don't validate streamed responses (#1259)
    * rework PR #467 - don't attempt to validate streamed responses
    
    * Add is_streamed property to ConnexionResponse
    
    * Adhere to response.direct_passthrough
    
    * Add test for file response validation
    
    * Add warning about skipping validation for streamed response
    
    Co-authored-by: Robbe Sneyders <robbe.sneyders@ml6.eu>

--- connexion/decorators/response.py
+++ connexion/decorators/response.py
@@ -88,9 +88,11 @@ class ResponseValidator(BaseDecorator):
         def _wrapper(request, response):
             connexion_response = \
                 self.operation.api.get_connexion_response(response, self.mimetype)
-            self.validate_response(
-                connexion_response.body, connexion_response.status_code,
-                connexion_response.headers, request.url)
+            if not connexion_response.is_streamed:
+                logger.warning("Skipping response validation for streamed response.")
+                self.validate_response(
+                    connexion_response.body, connexion_response.status_code,
+                    connexion_response.headers, request.url)
 
             return response
 
