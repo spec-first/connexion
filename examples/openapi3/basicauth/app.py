cd68cb8185c7468f39557160b5ed7914860d75da
Andrey Abramov <a.abramov@rbkmoney.com>
2022-03-15 20:10:55 +0300

    Remove obsolete code

--- examples/openapi3/basicauth/app.py
+++ examples/openapi3/basicauth/app.py
@@ -4,8 +4,6 @@ Basic example of a resource server
 '''
 
 import connexion
-from connexion.decorators.security import validate_scope
-from connexion.exceptions import OAuthScopeProblem
 
 
 def basic_auth(username, password, required_scopes=None):
@@ -16,14 +14,6 @@ def basic_auth(username, password, required_scopes=None):
     else:
         # optional: raise exception for custom error response
         return None
-
-    # optional
-    if required_scopes is not None and not validate_scope(required_scopes, info['scope']):
-        raise OAuthScopeProblem(
-                description='Provided user doesn\'t have the required access rights',
-                required_scopes=required_scopes,
-                token_scopes=info['scope']
-            )
 
     return info
 
