7207c022dfef89f96e142271da8c32f3a17da427
Andrey Abramov <a.abramov@rbkmoney.com>
2022-03-15 20:23:00 +0300

    Remove scopes completely

--- examples/openapi3/basicauth/app.py
+++ examples/openapi3/basicauth/app.py
@@ -5,18 +5,15 @@ Basic example of a resource server
 
 import connexion
 
+PASSWD={
+    'admin': 'secret',
+    'foo': 'bar'
+}
 
 def basic_auth(username, password, required_scopes=None):
-    if username == 'admin' and password == 'secret':
-        info = {'sub': 'admin', 'scope': 'secret'}
-    elif username == 'foo' and password == 'bar':
-        info = {'sub': 'user1', 'scope': ''}
-    else:
-        # optional: raise exception for custom error response
-        return None
-
-    return info
-
+    if PASSWD.get(username) == password:
+        return {'sub': username}
+    return None
 
 def get_secret(user) -> str:
     return f"You are {user} and the secret is 'wbevuec'"
