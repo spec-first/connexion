87a0fed4dcd164eb779cd11afd838845910b1e32
Andrey Abramov <nailus.wylde@gmail.com>
2022-03-16 22:20:44 +0300

    Remove obsolete code from basicauth example (#1486)
    * Remove obsolete code
    
    * Remove scopes completely
    
    * Add hinting
    
    * Remove outdated function from docs/security
    
    * Revert "Add hinting"
    
    This reverts commit faeea24c28bf88eead4015feea8f4dbd750cec1c.
    
    * Change README for examples and other review fixes

--- examples/swagger2/basicauth/app.py
+++ examples/swagger2/basicauth/app.py
@@ -5,14 +5,16 @@ Basic example of a resource server
 
 import connexion
 
+PASSWD = {
+    'admin': 'secret',
+    'foo': 'bar'
+}
 
-def basic_auth(username, password, required_scopes=None):
-    if username == 'admin' and password == 'secret':
-        return {'sub': 'admin'}
-
+def basic_auth(username, password):
+    if PASSWD.get(username) == password:
+        return {'sub': username}
     # optional: raise exception for custom error response
     return None
-
 
 def get_secret(user) -> str:
     return f"You are {user} and the secret is 'wbevuec'"
