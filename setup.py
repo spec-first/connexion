7b3f8839e8ebd6b6d5fbed71a3fa247c023c671f
Robbe Sneyders <robbe.sneyders@ml6.eu>
2022-03-18 08:26:29 +0100

    Add httpx dependency

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ install_requires = [
     'inflection>=0.3.1,<0.6',
     'werkzeug>=1.0,<3',
     'starlette>=0.15,<1',
+    'httpx>=0.15,<1',
 ]
 
 swagger_ui_require = 'swagger-ui-bundle>=0.0.2,<0.1'
