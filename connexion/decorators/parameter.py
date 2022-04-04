0b509f29a04ad2704baa46b3095d913107ffddf6
Robbe Sneyders <robbe.sneyders@ml6.eu>
2022-04-04 18:51:31 +0200

    Fix uri parsing for query parameter with empty brackets (#1501)

--- connexion/decorators/parameter.py
+++ connexion/decorators/parameter.py
@@ -48,6 +48,17 @@ def snake_and_shadow(name):
     return snake
 
 
+def sanitized(name):
+    return name and re.sub('^[^a-zA-Z_]+', '',
+                           re.sub('[^0-9a-zA-Z_]', '',
+                                  re.sub(r'\[(?!])', '_', name)))
+
+
+def pythonic(name):
+    name = name and snake_and_shadow(name)
+    return sanitized(name)
+
+
 def parameter_to_arg(operation, function, pythonic_params=False,
                      pass_context_arg_name=None):
     """
@@ -64,13 +75,6 @@ def parameter_to_arg(operation, function, pythonic_params=False,
     :type pass_context_arg_name: str|None
     """
     consumes = operation.consumes
-
-    def sanitized(name):
-        return name and re.sub('^[^a-zA-Z_]+', '', re.sub('[^0-9a-zA-Z[_]', '', re.sub(r'[\[]', '_', name)))
-
-    def pythonic(name):
-        name = name and snake_and_shadow(name)
-        return sanitized(name)
 
     sanitize = pythonic if pythonic_params else sanitized
     arguments, has_kwargs = inspect_function_arguments(function)
