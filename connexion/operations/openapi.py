e8734223919e7884f57f6596f2a2ac422e4dfa8d
Motti Lanzkron <motti@microfocus.com>
2022-06-09 11:30:12 +0300

    fix truncated comment

--- connexion/operations/openapi.py
+++ connexion/operations/openapi.py
@@ -254,7 +254,7 @@ class OpenAPIOperation(AbstractOperation):
         if len(arguments) <= 0 and not has_kwargs:
             return {}
 
-        # get the deprecated name from the body-schema for legacy connexion co
+        # get the deprecated name from the body-schema for legacy connexion compat
         x_body_name = sanitize(self.body_schema.get('x-body-name'))
         if x_body_name:
             warnings.warn('x-body-name within the requestBody schema will be deprecated in the '
