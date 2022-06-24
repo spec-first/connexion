3c6e13c1c1e4245d301b438eab148a058a159fe9
Nico Braun <39703898+bluebrown@users.noreply.github.com>
2022-06-24 17:55:11 +0200

    call as_view in methodresolver (#1552)
    * call as_view in methodresolver
    
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>
    
    * allow to use deprecated method view resolver
    
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>
    
    * update method view example
    
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>
    
    * update method view docs
    
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>
    
    * fix pre-commit issues
    
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- examples/openapi3/methodresolver/app.py
+++ examples/openapi3/methodresolver/app.py
@@ -6,12 +6,36 @@ from connexion.resolver import MethodViewResolver
 
 logging.basicConfig(level=logging.INFO)
 
-if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, specification_dir='openapi/', debug=True)
+zoo = {
+    1: {
+        "id": 1,
+        "name": "giraffe",
+        "tags": ["africa", "yellow", "hoofs", "herbivore", "long neck"],
+    },
+    2: {
+        "id": 2,
+        "name": "lion",
+        "tags": ["africa", "yellow", "paws", "carnivore", "mane"],
+    },
+}
+
+if __name__ == "__main__":
+    app = connexion.FlaskApp(__name__, specification_dir="openapi/", debug=True)
 
     options = {"swagger_ui": True}
-    app.add_api('pets-api.yaml',
-                options=options,
-                arguments={'title': 'MethodViewResolver Example'},
-                resolver=MethodViewResolver('api'), strict_validation=True, validate_responses=True )
-    app.run(port=9090)
+    app.add_api(
+        "pets-api.yaml",
+        options=options,
+        arguments={"title": "MethodViewResolver Example"},
+
+        resolver=MethodViewResolver(
+            "api", 
+            # class params are entirely optional
+            # they allow to inject dependencies top down
+            # so that the app can be wired, in the entrypoint
+            class_arguments={"PetsView": {"kwargs": {"pets": zoo}}},
+        ),
+        strict_validation=True,
+        validate_responses=True,
+    )
+    app.run(port=9090, debug=True)
