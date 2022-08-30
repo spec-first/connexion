511de06f16a5d30a2df0d9fb8447e3aac84b5977
Ruwan <ruwanlambrichts@gmail.com>
2022-08-30 19:53:46 +0200

    WIP

--- connexion/middleware/validation.py
+++ connexion/middleware/validation.py
@@ -1,22 +1,22 @@
 """
 Validation Middleware.
 """
+import functools
 import logging
 import pathlib
 import typing as t
 
 from starlette.types import ASGIApp, Receive, Scope, Send
+from starlette.datastructures import ImmutableMultiDict, UploadFile
 
 from connexion.apis.abstract import AbstractSpecAPI
-from connexion.decorators.uri_parsing import AbstractURIParser
+from connexion.decorators.uri_parsing import AbstractURIParser, Swagger2URIParser, OpenAPIURIParser
 from connexion.exceptions import MissingMiddleware, ResolverError
 from connexion.http_facts import METHODS
-from connexion.lifecycle import MiddlewareRequest
+from connexion.lifecycle import ConnexionRequest, MiddlewareRequest
 from connexion.middleware import AppMiddleware
 from connexion.middleware.routing import ROUTING_CONTEXT
-from connexion.operations.abstract import AbstractOperation
-from connexion.operations.openapi import OpenAPIOperation
-from connexion.operations.swagger2 import Swagger2Operation
+from connexion.operations import AbstractOperation, OpenAPIOperation, Swagger2Operation
 
 from ..decorators.response import ResponseValidator
 from ..decorators.validation import ParameterValidator, RequestBodyValidator
@@ -58,7 +58,6 @@ class ValidationMiddleware(AppMiddleware):
                 "Could not find routing information in scope. Please make sure "
                 "you have a routing middleware registered upstream. "
             )
-
         api_base_path = connexion_context.get("api_base_path")
         if api_base_path:
             api = self.apis[api_base_path]
@@ -68,16 +67,33 @@ class ValidationMiddleware(AppMiddleware):
             except KeyError as e:
                 if operation_id is None:
                     logger.debug("Skipping validation check for operation without id.")
+                    await self.app(scope, receive, send)
+                    return
                 else:
                     raise MissingValidationOperation(
                         "Encountered unknown operation_id."
                     ) from e
             else:
                 # TODO: Add validation logic
-                request = MiddlewareRequest(scope)
-                await operation(request)
+                # messages = []
+                # async def wrapped_receive():
+                #     msg = await receive()
+                #     messages.append(msg)
+                #     return msg
+                # request = MiddlewareRequest(scope, wrapped_receive)
+                # if messages:
+                #     async def new_receive():
+                #         msg = messages.pop(0)
+                #         return msg
+                # else:
+                #     new_receive = receive
 
-        await self.app(scope, receive, send)
+                # await operation(request)
+                # await self.app(scope, new_receive, send)
+                await self.app(scope, receive, send)
+
+        else:
+            await self.app(scope, receive, send)
 
 
 class ValidationAPI(AbstractSpecAPI):
@@ -164,7 +180,12 @@ class AbstractValidationOperation:
         strict_validation = strict_validation
         self._validator_map = dict(VALIDATOR_MAP)
         self._validator_map.update(validator_map or {})
+        # TODO: Change URI parser class for middleware
         self._uri_parser_class = uri_parser_class
+        self._async_uri_parser_class = {
+            Swagger2URIParser: AsyncSwagger2URIParser,
+            OpenAPIURIParser: AsyncOpenAPIURIParser,
+        }.get(uri_parser_class, AsyncOpenAPIURIParser)
         self.validation_fn = self._get_validation_fn()
 
     @classmethod
@@ -204,7 +225,18 @@ class AbstractValidationOperation:
         async def function(request):
             pass
 
+        # function = self._uri_parsing_decorator(function)
+
         return function
+
+    @property
+    def _uri_parsing_decorator(self):
+        """
+        Returns a decorator that parses request data and handles things like
+        array types, and duplicate parameter definitions.
+        """
+        # TODO: Instantiate the class only once?
+        return self._async_uri_parser_class(self.parameters, self.body_definition)
 
     async def __call__(self, request):
         await self.validation_fn(request)
@@ -254,3 +286,76 @@ class OpenAPIValidationOperation(AbstractValidationOperation):
 
 class MissingValidationOperation(Exception):
     """Missing validation operation"""
+
+
+class AbstractAsyncURIParser(AbstractURIParser):
+    """URI Parser with support for async requests."""
+
+    def __call__(self, function):
+        """
+        :type function: types.FunctionType
+        :rtype: types.FunctionType
+        """
+
+        @functools.wraps(function)
+        async def wrapper(request: MiddlewareRequest):
+            def coerce_dict(md):
+                """MultiDict -> dict of lists"""
+                if isinstance(md, ImmutableMultiDict):
+                    # Starlette MultiDict doesn't have same interface as werkzeug one
+                    return {k: md.getlist(k) for k in md}
+                try:
+                    return md.to_dict(flat=False)
+                except AttributeError:
+                    return dict(md.items())
+
+            query = coerce_dict(request.query_params)
+            path_params = coerce_dict(request.path_params)
+            # FIXME
+            # Read JSON first to try circumvent stream consumed error (because form doesn't story anything in self._body)
+            # Potential alternative: refactor such that parsing/validation only calls the methods/properties when necessary
+            try:
+                json = await request.json()
+            except ValueError as e:
+                json = None
+            # Flask splits up file uploads and text input in `files` and `form`,
+            # while starlette puts them both in `form`
+            form = await request.form()
+            form = coerce_dict(form)
+            form_parameters = {k: v for k, v in form.items() if isinstance(v, str)}
+            form_files = {k: v for k, v in form.items() if isinstance(v, UploadFile)}
+            for v in form.values():
+                if not isinstance(v, (list, str, UploadFile)):
+                    raise TypeError(f"Unexpected type in form: {type(v)} with value: {v}")
+
+            # Construct ConnexionRequest
+
+            request = ConnexionRequest(
+                url=str(request.url),
+                method=request.method,
+                path_params=self.resolve_path(path_params),
+                query=self.resolve_query(query),
+                headers=request.headers,
+                form=self.resolve_form(form_parameters),
+                body=await request.body(),
+                json_getter=lambda: json,
+                files=form_files,
+                context=request.context,
+                cookies=request.cookies,
+            )
+            request.query = self.resolve_query(query)
+            request.path_params = self.resolve_path(path_params)
+            request.form = self.resolve_form(form)
+
+            response = await function(request)
+            return response
+
+        return wrapper
+
+
+class AsyncSwagger2URIParser(AbstractAsyncURIParser, Swagger2URIParser):
+    """Swagger2URIParser with support for async requests."""
+
+
+class AsyncOpenAPIURIParser(AbstractAsyncURIParser, OpenAPIURIParser):
+    """OpenAPIURIParser with support for async requests."""
