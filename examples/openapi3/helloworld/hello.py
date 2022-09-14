d4c8ec27bc383c02408efdeed40559c9c471ad7d
Robbe Sneyders <robbe.sneyders@ml6.eu>
2022-09-14 22:46:06 +0200

    Revert example changes

--- examples/openapi3/helloworld/hello.py
+++ examples/openapi3/helloworld/hello.py
@@ -3,12 +3,11 @@
 import connexion
 
 
-def post_greeting(body: dict) -> str:
-    print(body)
-    return f"Hello {body['name']}"
+def post_greeting(name: str) -> str:
+    return f"Hello {name}"
 
 
 if __name__ == "__main__":
-    app = connexion.App(__name__, port=9090, specification_dir="openapi/")
+    app = connexion.FlaskApp(__name__, port=9090, specification_dir="openapi/")
     app.add_api("helloworld-api.yaml", arguments={"title": "Hello World Example"})
     app.run()
