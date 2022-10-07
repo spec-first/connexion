b8bdcc999d52144fa90cf276cd1702c7e8f422d2
Robbe Sneyders <robbe.sneyders@gmail.com>
2022-10-07 08:02:57 +0200

    Move Swagger 2 form validation to middleware (#1599)
    * Move Swagger 2 form validation to middleware
    
    * Add unit test for form transformation

--- connexion/operations/abstract.py
+++ connexion/operations/abstract.py
@@ -280,16 +280,14 @@ class AbstractOperation(metaclass=abc.ABCMeta):
         Content-Types that the operation consumes
         """
 
-    @property
     @abc.abstractmethod
-    def body_schema(self):
+    def body_schema(self, content_type: str = None) -> dict:
         """
         The body schema definition for this operation.
         """
 
-    @property
     @abc.abstractmethod
-    def body_definition(self):
+    def body_definition(self, content_type: str = None) -> dict:
         """
         The body definition for this operation.
         :rtype: dict
@@ -371,7 +369,7 @@ class AbstractOperation(metaclass=abc.ABCMeta):
         Returns a decorator that parses request data and handles things like
         array types, and duplicate parameter definitions.
         """
-        return self._uri_parser_class(self.parameters, self.body_definition)
+        return self._uri_parser_class(self.parameters, self.body_definition())
 
     @property
     def function(self):
