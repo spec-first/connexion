a8295361e32253fe65c9b89f63d1ae278cb97582
enerqi <enerqi@users.noreply.github.com>
2022-12-07 06:59:08 +0000

    Flask apps only signal an exception on real server errors (#1611)
    Fixes the problem of non-exceptional "exceptions" being recorded by
    telemetry systems as a serious error. This expands on the changes made
    in #1326. The intention of that other change seems to be making
    telemetry systems like Sentry record serious errors. Diving into the
    Sentry implementation even shows that the signalled exception is
    recorded at "level" = "error".
    
    The root of the problem is that exceptions are being used for control
    flow, which is not ideal - convenient for app writers but not always for
    library maintainers. The connexion BadRequestProblem and
    NotFoundProblem, for example, should not be recorded as an error in a
    telemetry system. In my case
    [elastic-apm-python](https://github.com/elastic/apm-agent-python) is
    receiving these signals and recording 4xx events as serious errors.
    
    This pull request only propagates an exception signal to flask if it's a
    serious error.
    
    Aiohttp applications have a similar problem with exceptions being used
    for control flow - the problems middleware will convert the exception
    into an appropriate problem response but things like the elastic apm
    python telemetry middleware will see that exception and record it as a
    serious error. Interestingly aiohttp also uses exceptions for control
    flow and the elastic apm agent was patched to specifically ignore aio
    web exceptions below the 5xx status response range. Elastic apm and
    sentry cannot be expected to be aware of non-serious control flow
    exceptions used by various libraries though. So, a solution for Aiohttp
    applications is a separate problem.
    
    Co-authored-by: Enerqi <>

--- connexion/apps/flask_app.py
+++ connexion/apps/flask_app.py
@@ -63,7 +63,6 @@ class FlaskApp(AbstractApp):
         """
         :type exception: Exception
         """
-        signals.got_request_exception.send(self.app, exception=exception)
         if isinstance(exception, ProblemException):
             response = problem(
                 status=exception.status,
@@ -84,6 +83,9 @@ class FlaskApp(AbstractApp):
                 status=exception.code,
                 headers=exception.get_headers(),
             )
+
+        if response.status_code >= 500:
+            signals.got_request_exception.send(self.app, exception=exception)
 
         return FlaskApi.get_response(response)
 
