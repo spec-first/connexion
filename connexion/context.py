dc8ed3e8ecd38fe129fa42ddbbe60c458e6ed1bc
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-02-15 18:05:46 +0100

    Change connexion.request to ASGIRequest instead of a flask request

--- connexion/context.py
+++ connexion/context.py
@@ -3,6 +3,7 @@ from contextvars import ContextVar
 from starlette.types import Receive, Scope
 from werkzeug.local import LocalProxy
 
+from connexion.lifecycle import ASGIRequest
 from connexion.operations import AbstractOperation
 
 UNBOUND_MESSAGE = (
@@ -22,3 +23,7 @@ receive = LocalProxy(_receive, unbound_message=UNBOUND_MESSAGE)
 
 _scope: ContextVar[Scope] = ContextVar("SCOPE")
 scope = LocalProxy(_scope, unbound_message=UNBOUND_MESSAGE)
+
+request = LocalProxy(
+    lambda: ASGIRequest(scope, receive), unbound_message=UNBOUND_MESSAGE
+)
