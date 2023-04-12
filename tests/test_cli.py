bfb4ec7d7e808a6bceb30ebf9f3f6572c031fe1b
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-04-13 00:23:03 +0200

    Workaround for failing cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,6 @@
 import logging
 from unittest.mock import MagicMock
 
-import connexion
 import importlib_metadata
 import pytest
 from click.testing import CliRunner
@@ -11,13 +10,27 @@ from connexion.exceptions import ResolverError
 from conftest import FIXTURES_FOLDER
 
 
-@pytest.fixture()
-def mock_app_run(mock_get_function_from_name):
-    test_server = MagicMock(wraps=connexion.FlaskApp(__name__))
-    test_server.run = MagicMock(return_value=True)
-    test_app = MagicMock(return_value=test_server)
-    mock_get_function_from_name.return_value = test_app
-    return test_app
+@pytest.fixture(scope="function")
+def mock_app_run(app_class, monkeypatch):
+    mocked_app = MagicMock(name="mocked_app", wraps=app_class(__name__))
+
+    def mocked_run(*args, **kwargs):
+        mocked_app.middleware._build_middleware_stack()
+
+    mocked_app.run = MagicMock(name="mocked_app.run", side_effect=mocked_run)
+
+    def get_mocked_app(*args, **kwargs):
+        return mocked_app
+
+    mocked_app_class = MagicMock(name="mocked_app_class", side_effect=get_mocked_app)
+
+    def get_mocked_app_class(*args, **kwargs):
+        return mocked_app_class
+
+    monkeypatch.setattr(
+        "connexion.cli.connexion.utils.get_function_from_name", get_mocked_app_class
+    )
+    return mocked_app_class
 
 
 @pytest.fixture()
@@ -170,23 +183,34 @@ def test_run_using_option_base_path(mock_app_run, expected_arguments, spec_file)
         validate_responses=False,
         strict_validation=False,
     )
-    mock_app_run().add_api.assert_called_with(spec_file, **expected_arguments)
+    app_instance = mock_app_run()
+    app_instance.add_api.assert_called_with(spec_file, **expected_arguments)
 
 
-def test_run_unimplemented_operations_and_stub(mock_app_run):
+def test_run_unimplemented_operations(mock_app_run):
     runner = CliRunner()
 
     spec_file = str(FIXTURES_FOLDER / "missing_implementation/swagger.yaml")
     with pytest.raises(ResolverError):
         runner.invoke(main, ["run", spec_file], catch_exceptions=False)
-    # yet can be run with --stub option
-    result = runner.invoke(main, ["run", spec_file, "--stub"], catch_exceptions=False)
-    assert result.exit_code == 0
 
     spec_file = str(FIXTURES_FOLDER / "module_does_not_exist/swagger.yaml")
     with pytest.raises(ResolverError):
         runner.invoke(main, ["run", spec_file], catch_exceptions=False)
-    # yet can be run with --stub option
+
+
+def test_run_unimplemented_operations_with_stub1(mock_app_run):
+    runner = CliRunner()
+
+    spec_file = str(FIXTURES_FOLDER / "missing_implementation/swagger.yaml")
+    result = runner.invoke(main, ["run", spec_file, "--stub"], catch_exceptions=False)
+    assert result.exit_code == 0
+
+
+def test_run_unimplemented_operations_with_stub2(mock_app_run):
+    runner = CliRunner()
+
+    spec_file = str(FIXTURES_FOLDER / "module_does_not_exist/swagger.yaml")
     result = runner.invoke(main, ["run", spec_file, "--stub"], catch_exceptions=False)
     assert result.exit_code == 0
 
@@ -195,9 +219,6 @@ def test_run_unimplemented_operations_and_mock(mock_app_run):
     runner = CliRunner()
 
     spec_file = str(FIXTURES_FOLDER / "missing_implementation/swagger.yaml")
-    with pytest.raises(ResolverError):
-        runner.invoke(main, ["run", spec_file], catch_exceptions=False)
-    # yet can be run with --mock option
     result = runner.invoke(
         main, ["run", spec_file, "--mock=all"], catch_exceptions=False
     )
