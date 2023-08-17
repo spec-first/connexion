a158d66c0bd2e288da03b599204b6eeb9c88f873
莘权 马 <mashenquan@fuzhi.ai>
2023-08-17 18:01:33 +0800

    fixbug: The JSON data of requests.post has been incorrectly discarded.

--- connexion/operations/abstract.py
+++ connexion/operations/abstract.py
@@ -277,6 +277,7 @@ class AbstractOperation(SecureOperation, metaclass=abc.ABCMeta):
         """
         ret = {}
         ret.update(self._get_path_arguments(path_params, sanitize))
+        ret.update(self._get_query_arguments(body, sanitize))
         ret.update(self._get_query_arguments(query_params, arguments,
                                              has_kwargs, sanitize))
 
