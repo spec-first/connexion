b8d57d341817e11ffecd45145b112b099ff77c5b
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-10-11 23:34:32 +0200

    Limit the docs TOC depth to 2

--- docs/conf.py
+++ docs/conf.py
@@ -110,8 +110,11 @@ try:
 
     html_theme = "sphinx_rtd_theme"
 
-
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+    html_theme_options = {
+        'navigation_depth': 2
+    }
 
     html_context = {
         'display_github': True,
