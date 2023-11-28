3bac45d4deadfe28f74b8d45c364a46b527e563f
Niels Dewulf <87133686+nielsbox@users.noreply.github.com>
2023-11-28 16:48:09 +0100

    Cleanup

--- connexion/middleware/server_error.py
+++ connexion/middleware/server_error.py
@@ -1,52 +1,15 @@
-import asyncio
-import functools
 import logging
-import typing as t
 
-from starlette.concurrency import run_in_threadpool
 from starlette.middleware.errors import (
     ServerErrorMiddleware as StarletteServerErrorMiddleware,
 )
 from starlette.requests import Request as StarletteRequest
-from starlette.responses import Response as StarletteResponse
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 from connexion.exceptions import InternalServerError
-from connexion.lifecycle import ConnexionRequest, ConnexionResponse
-from connexion.types import MaybeAwaitable
+from connexion.lifecycle import ConnexionResponse
 
 logger = logging.getLogger(__name__)
-
-
-def connexion_wrapper(
-    handler: t.Callable[
-        [ConnexionRequest, Exception], MaybeAwaitable[ConnexionResponse]
-    ]
-) -> t.Callable[[StarletteRequest, Exception], t.Awaitable[StarletteResponse]]:
-    """Wrapper that translates Starlette requests to Connexion requests before passing
-    them to the error handler, and translates the returned Connexion responses to
-    Starlette responses."""
-
-    @functools.wraps(handler)
-    async def wrapper(request: StarletteRequest, exc: Exception) -> StarletteResponse:
-        request = ConnexionRequest.from_starlette_request(request)
-
-        if asyncio.iscoroutinefunction(handler):
-            response = await handler(request, exc)  # type: ignore
-        else:
-            response = await run_in_threadpool(handler, request, exc)
-
-        while asyncio.iscoroutine(response):
-            response = await response
-
-        return StarletteResponse(
-            content=response.body,
-            status_code=response.status_code,
-            media_type=response.mimetype,
-            headers=response.headers,
-        )
-
-    return wrapper
 
 
 class ServerErrorMiddleware(StarletteServerErrorMiddleware):
