46e71912343110febf520f96b56bf5aa619303f5
Niels Dewulf <87133686+nielsbox@users.noreply.github.com>
2023-11-29 09:27:46 +0100

    Fix ServerError Middleware

--- connexion/middleware/server_error.py
+++ connexion/middleware/server_error.py
@@ -3,11 +3,7 @@ import logging
 from starlette.middleware.errors import (
     ServerErrorMiddleware as StarletteServerErrorMiddleware,
 )
-from starlette.requests import Request as StarletteRequest
 from starlette.types import ASGIApp, Receive, Scope, Send
-
-from connexion.exceptions import InternalServerError
-from connexion.lifecycle import ConnexionResponse
 
 logger = logging.getLogger(__name__)
 
@@ -18,12 +14,6 @@ class ServerErrorMiddleware(StarletteServerErrorMiddleware):
 
     def __init__(self, next_app: ASGIApp):
         super().__init__(next_app)
-
-    @staticmethod
-    def error_response(_request: StarletteRequest, exc: Exception) -> ConnexionResponse:
-        """Default handler for any unhandled Exception"""
-        logger.error("%r", exc, exc_info=exc)
-        return InternalServerError().to_problem()
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         await super().__call__(scope, receive, send)
