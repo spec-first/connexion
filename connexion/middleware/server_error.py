60791c234e9e9d8ce8e26d65da714699455678d0
Niels Dewulf <87133686+nielsbox@users.noreply.github.com>
2023-11-30 13:36:14 +0100

    Address comment

--- connexion/middleware/server_error.py
+++ connexion/middleware/server_error.py
@@ -1,12 +1,32 @@
+import logging
+import typing as t
+
 from starlette.middleware.errors import (
     ServerErrorMiddleware as StarletteServerErrorMiddleware,
 )
+from starlette.requests import Request as StarletteRequest
 from starlette.types import ASGIApp
+
+from connexion.exceptions import InternalServerError
+from connexion.middleware.exceptions import connexion_wrapper
+from connexion.lifecycle import ConnexionRequest, ConnexionResponse
+
+logger = logging.getLogger(__name__)
 
 
 class ServerErrorMiddleware(StarletteServerErrorMiddleware):
     """Subclass of starlette ServerErrorMiddleware to change handling of Unhandled Server
     exceptions to existing connexion behavior."""
 
-    def __init__(self, next_app: ASGIApp):
-        super().__init__(next_app)
+    def __init__(self, next_app: ASGIApp, handler: t.Optional[
+            t.Callable[[ConnexionRequest, Exception], t.Any]] = None):
+        super().__init__(next_app, handler=connexion_wrapper(handler or self.error_response))
+
+    @staticmethod
+    def error_response(
+        _request: StarletteRequest, exc: Exception
+    ) -> ConnexionResponse:
+        """Default handler for any unhandled Exception"""
+        logger.error("%r", exc, exc_info=exc)
+        return InternalServerError().to_problem()
+
