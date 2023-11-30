918d58cb0e6bd41954b1d55cc6e38bf294412853
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-11-30 23:46:06 +0100

    Small typing fixes

--- connexion/middleware/server_error.py
+++ connexion/middleware/server_error.py
@@ -4,12 +4,12 @@ import typing as t
 from starlette.middleware.errors import (
     ServerErrorMiddleware as StarletteServerErrorMiddleware,
 )
-from starlette.requests import Request as StarletteRequest
 from starlette.types import ASGIApp
 
 from connexion.exceptions import InternalServerError
 from connexion.lifecycle import ConnexionRequest, ConnexionResponse
 from connexion.middleware.exceptions import connexion_wrapper
+from connexion.types import MaybeAwaitable
 
 logger = logging.getLogger(__name__)
 
@@ -21,14 +21,16 @@ class ServerErrorMiddleware(StarletteServerErrorMiddleware):
     def __init__(
         self,
         next_app: ASGIApp,
-        handler: t.Optional[t.Callable[[ConnexionRequest, Exception], t.Any]] = None,
+        handler: t.Optional[
+            t.Callable[[ConnexionRequest, Exception], MaybeAwaitable[ConnexionResponse]]
+        ] = None,
     ):
         handler = connexion_wrapper(handler) if handler else None
         super().__init__(next_app, handler=handler)
 
     @staticmethod
     @connexion_wrapper
-    def error_response(_request: StarletteRequest, exc: Exception) -> ConnexionResponse:
+    def error_response(_request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
         """Default handler for any unhandled Exception"""
         logger.error("%r", exc, exc_info=exc)
         return InternalServerError().to_problem()
