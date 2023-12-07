622f247fa063c7954d944c0b28bfec2a4fd00dc2
Niels Dewulf <87133686+nielsbox@users.noreply.github.com>
2023-12-07 12:32:13 +0100

    Add tests

---
+++ tests/api/test_swagger_ui.py
@@ -0,0 +1,31 @@
+from connexion.middleware.swagger_ui import SwaggerUIConfig
+
+
+def test_swagger(simple_app):
+    app_client = simple_app.test_client()
+    for api in simple_app.middleware.apis:
+        if api.kwargs['swagger_ui_options']:
+            swagger_ui_options = api.kwargs['swagger_ui_options']
+        else:
+            swagger_ui_options = simple_app.middleware.options.swagger_ui_options
+
+        options = SwaggerUIConfig(
+            swagger_ui_options, oas_version=api.specification.version
+        )
+        response = app_client.get(api.specification.base_path+options.openapi_spec_path)
+        assert response.status_code == 200
+
+
+def test_openapi(simple_openapi_app):
+    app_client = simple_openapi_app.test_client()
+    for api in simple_openapi_app.middleware.apis:
+        if api.kwargs['swagger_ui_options']:
+            swagger_ui_options = api.kwargs['swagger_ui_options']
+        else:
+            swagger_ui_options = simple_openapi_app.middleware.options.swagger_ui_options
+
+        options = SwaggerUIConfig(
+            swagger_ui_options, oas_version=api.specification.version
+        )
+        response = app_client.get(api.specification.base_path+options.openapi_spec_path)
+        assert response.status_code == 200
