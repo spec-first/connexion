65e73924af5eaa92d62245a4049f0783fb91b512
Niels Dewulf <87133686+nielsbox@users.noreply.github.com>
2023-12-07 12:34:34 +0100

    Fix naming

--- tests/api/test_swagger_ui.py
+++ tests/api/test_swagger_ui.py
@@ -1,7 +1,7 @@
 from connexion.middleware.swagger_ui import SwaggerUIConfig
 
 
-def test_swagger(simple_app):
+def test_simple(simple_app):
     app_client = simple_app.test_client()
     for api in simple_app.middleware.apis:
         if api.kwargs['swagger_ui_options']:
@@ -16,7 +16,7 @@ def test_swagger(simple_app):
         assert response.status_code == 200
 
 
-def test_openapi(simple_openapi_app):
+def test_simple_openapi(simple_openapi_app):
     app_client = simple_openapi_app.test_client()
     for api in simple_openapi_app.middleware.apis:
         if api.kwargs['swagger_ui_options']:
