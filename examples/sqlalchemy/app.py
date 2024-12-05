c3fd66faea8e343ce40e5a749dc0d4250a3974b4
Christopher Lott <chrisinmtown@users.noreply.github.com>
2024-12-05 16:48:34 -0500

    Update example for latest version of SQLAlchemy
    Pass argument that disables SQLite thread check when creating the engine
    Revise request-processing functions to use a database session context
    Drop TODO from the example README

--- examples/sqlalchemy/app.py
+++ examples/sqlalchemy/app.py
@@ -5,58 +5,60 @@ import connexion
 import orm
 from connexion import NoContent
 
-db_session = None
-
 
 def get_pets(limit, animal_type=None):
-    q = db_session.query(orm.Pet)
-    if animal_type:
-        q = q.filter(orm.Pet.animal_type == animal_type)
-    return [p.dump() for p in q][:limit]
+    with db_session_factory() as db_session:
+        q = db_session.query(orm.Pet)
+        if animal_type:
+            q = q.filter(orm.Pet.animal_type == animal_type)
+        return [p.dump() for p in q][:limit]
 
 
 def get_pet(pet_id):
-    pet = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
-    return pet.dump() if pet is not None else ("Not found", 404)
+    with db_session_factory() as db_session:
+        pet = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
+        return pet.dump() if pet is not None else ("Not found", 404)
 
 
 def put_pet(pet_id, pet):
-    p = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
-    pet["id"] = pet_id
-    if p is not None:
-        logging.info("Updating pet %s..", pet_id)
-        p.update(**pet)
-    else:
-        logging.info("Creating pet %s..", pet_id)
-        pet["created"] = datetime.datetime.utcnow()
-        db_session.add(orm.Pet(**pet))
-    db_session.commit()
-    return NoContent, (200 if p is not None else 201)
+    with db_session_factory() as db_session:
+        p = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
+        pet["id"] = pet_id
+        if p is not None:
+            logging.info("Updating pet %s..", pet_id)
+            p.update(**pet)
+        else:
+            logging.info("Creating pet %s..", pet_id)
+            pet["created"] = datetime.datetime.now(datetime.UTC)
+            db_session.add(orm.Pet(**pet))
+        db_session.commit()
+        return NoContent, (200 if p is not None else 201)
 
 
 def delete_pet(pet_id):
-    pet = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
-    if pet is not None:
-        logging.info("Deleting pet %s..", pet_id)
-        db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).delete()
-        db_session.commit()
-        return NoContent, 204
-    else:
-        return NoContent, 404
+    with db_session_factory() as db_session:
+        pet = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
+        if pet is not None:
+            logging.info("Deleting pet %s..", pet_id)
+            db_session.delete(pet)
+            db_session.commit()
+            return NoContent, 204
+        else:
+            return NoContent, 404
 
 
 logging.basicConfig(level=logging.INFO)
-db_session = orm.init_db("sqlite:///:memory:")
+db_session_factory = orm.init_db()
+pets = {
+    1: {"name": "Aldo", "animal_type": "cat"},
+    2: {"name": "Bailey", "animal_type": "dog"},
+    3: {"name": "Hugo", "animal_type": "cat"},
+}
+for id_, pet in pets.items():
+    put_pet(id_, pet)
 app = connexion.FlaskApp(__name__, specification_dir="spec")
 app.add_api("openapi.yaml")
 app.add_api("swagger.yaml")
-
-application = app.app
-
-
-@application.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
 
 
 if __name__ == "__main__":
