c50000589f7227f8f783185f7377b6bda9cef5f2
Giulio <3272563+giuliohome@users.noreply.github.com>
2025-09-06 17:30:45 +0200

    indent

--- connexion/spec.py
+++ connexion/spec.py
@@ -204,38 +204,38 @@ class Specification(Mapping):
         return OpenAPISpecification(spec, base_uri=base_uri)
 
 
-def clone(self):
-    # Check if spec contains only internal refs (starting with #)
-    # For external refs, we need the processed spec to maintain resolved content
-    if self._has_only_internal_refs():
-        return type(self)(copy.deepcopy(self._raw_spec))
-    else:
-        return type(self)(copy.deepcopy(self._spec))
+    def clone(self):
+        # Check if spec contains only internal refs (starting with #)
+        # For external refs, we need the processed spec to maintain resolved content
+        if self._has_only_internal_refs():
+            return type(self)(copy.deepcopy(self._raw_spec))
+        else:
+            return type(self)(copy.deepcopy(self._spec))
 
 
-def _has_only_internal_refs(self):
-    """Check if all $ref entries point to internal references only (starting with #)"""
+    def _has_only_internal_refs(self):
+        """Check if all $ref entries point to internal references only (starting with #)"""
 
-    def check_refs(obj):
-        if isinstance(obj, dict):
-            for key, value in obj.items():
-                if key == "$ref" and isinstance(value, str):
-                    # External ref if it doesn't start with # or contains file references
-                    if (
-                        not value.startswith("#")
-                        or ".yaml" in value
-                        or ".json" in value
-                    ):
+        def check_refs(obj):
+            if isinstance(obj, dict):
+                for key, value in obj.items():
+                    if key == "$ref" and isinstance(value, str):
+                        # External ref if it doesn't start with # or contains file references
+                        if (
+                            not value.startswith("#")
+                            or ".yaml" in value
+                            or ".json" in value
+                        ):
+                            return False
+                    elif not check_refs(value):
                         return False
-                elif not check_refs(value):
-                    return False
-        elif isinstance(obj, (list, tuple)):
-            for item in obj:
-                if not check_refs(item):
-                    return False
-        return True
+            elif isinstance(obj, (list, tuple)):
+                for item in obj:
+                    if not check_refs(item):
+                        return False
+            return True
 
-    return check_refs(self._raw_spec)
+        return check_refs(self._raw_spec)
 
     @classmethod
     def load(cls, spec, *, arguments=None):
